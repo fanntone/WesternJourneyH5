97c3a255616ed212144a31b5d44d167aba5f85d8
fanntone <fanntone@gmail.com>
2016-11-23 16:05:26 +0800

    Add 5 server.logic files.

---
+++ SlotGame/src/com/jinglei/game/server/logic/Logic_CGGSLMSG_MAX.java
@@ -0,0 +1,26 @@
+package com.jinglei.game.server.logic;
+
+import com.jinglei.channel.NettyClientChannel;
+import com.jinglei.server.logic.CommonLogic;
+
+public class Logic_CGGSLMSG_MAX implements CommonLogic {
+
+	@Override
+	public void executeLogic(byte[] arg0, NettyClientChannel arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getLogicName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CommonLogic newInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
