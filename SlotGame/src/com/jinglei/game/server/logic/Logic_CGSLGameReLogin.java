2da4c057b35499f1b57ca39d3ef71e570cfcba9c
fanntone <fanntone@gmail.com>
2016-11-23 16:40:08 +0800

    Implement class Logic_CGSLGameReLogin.

--- SlotGame/src/com/jinglei/game/server/logic/Logic_CGSLGameReLogin.java
+++ SlotGame/src/com/jinglei/game/server/logic/Logic_CGSLGameReLogin.java
@@ -1,26 +1,61 @@
 package com.jinglei.game.server.logic;
 
 import com.jinglei.channel.NettyClientChannel;
+import com.jinglei.game.SysLog;
 import com.jinglei.server.logic.CommonLogic;
+import java.nio.charset.StandardCharsets;
+import com.alibaba.fastjson.JSON;
+import com.jinglei.packets.ctos.CGSLGameReLogin;
+import com.jinglei.packets.stoc.CGCliGameReLogin;
 
 public class Logic_CGSLGameReLogin implements CommonLogic {
 
+	public Logic_CGSLGameReLogin() {
+		super();
+	}
+	
 	@Override
-	public void executeLogic(byte[] arg0, NettyClientChannel arg1) {
-		// TODO Auto-generated method stub
+	public void executeLogic(byte[] packet_data, NettyClientChannel channel) {
+		try	{
+			SysLog.PrintError("Logic_CGSLGameReLogin Run !!");
+			
+			if ( packet_data != null && channel != null ) {
+				String json_text = new String(packet_data, StandardCharsets.UTF_8);
+				CGSLGameReLogin receive = JSON.parseObject(json_text, CGSLGameReLogin.class);
+				
+				if ( receive != null && channel != null) {
+					CGCliGameReLogin responses = new CGCliGameReLogin();
+					
+					if ( responses != null ) {
+						String json2string = JSON.toJSONString(responses,
+															   new com.alibaba.fastjson.serializer.PascalNameFilter());
+						SysLog.PrintInfo(String.format("Logic_CGSLGameReLogin Command:%s WriteJSON:[%s]!!",
+										 "CGCliGameReLogin",
+										 json2string));
+						channel.writeJSON(json2string);
+					}
+				}	
+			}	
+		}
+		catch(Exception e) {
+			
+		}
+		finally {
+			SysLog.PrintError("Logic_CGSLGameReLogin Run finally!!");
+		}
 
 	}
 
 	@Override
 	public String getLogicName() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String(this.getClass().getName());
 	}
 
 	@Override
 	public CommonLogic newInstance() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Logic_CGSLGameReLogin();
 	}
 
 }
---
+++ SlotGame/src/com/jinglei/packets/stoc/CGCliGameReLogin.java
@@ -0,0 +1,13 @@
+package com.jinglei.packets.stoc;
+
+import com.jinglei.server.logic.JSONBean;
+
+public class CGCliGameReLogin implements JSONBean {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
