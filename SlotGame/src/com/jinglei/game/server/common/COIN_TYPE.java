a9f18286b301508c2d03da7547f9b1265d86bb52
fanntone <fanntone@gmail.com>
2016-11-24 17:47:57 +0800

    Fix and refactor some code.

--- SlotGame/src/com/jinglei/game/server/common/COIN_TYPE.java
+++ SlotGame/src/com/jinglei/game/server/common/COIN_TYPE.java
@@ -2,7 +2,7 @@ package com.jinglei.game.server.common;
 
 
 public enum COIN_TYPE {
-    COIN_TYPE_V_COIN,										///< 有價幣
-    COIN_TYPE_F_COIN,										///< 免費幣
-    COIN_TYPE_MAX,
+	COIN_TYPE_V_COIN,										///< 有價幣
+	COIN_TYPE_F_COIN,										///< 免費幣
+	COIN_TYPE_MAX,
 }
--- SlotGame/src/com/jinglei/game/server/common/PL_STATE.java
+++ SlotGame/src/com/jinglei/game/server/common/PL_STATE.java
@@ -1,11 +1,11 @@
 package com.jinglei.game.server.common;
 
 public enum PL_STATE {
-	   PL_STATE_READY_GAME,           															///< 已成局並且等待遊戲開始進行中
-	   PL_STATE_GET_DB_POINTS,																	///< 已取得DBSrv中的玩家點數並且DB已設定該玩家登入了遊戲伺服器
-	   PL_STATE_GS_CONNECTED,         															///< Socket已連線至GS
-	   PL_STATE_RUN_GAME,             															///< 遊戲中
-	   PL_STATE_QUIT_QUEUE,           															///< 離開局中
-	   PL_STATE_LOGOUT_GS,																											///< 登出GS
-	   PL_STATE_MAX,
+	PL_STATE_READY_GAME,           															///< 已成局並且等待遊戲開始進行中
+	PL_STATE_GET_DB_POINTS,																	///< 已取得DBSrv中的玩家點數並且DB已設定該玩家登入了遊戲伺服器
+	PL_STATE_GS_CONNECTED,         															///< Socket已連線至GS
+	PL_STATE_RUN_GAME,             															///< 遊戲中
+	PL_STATE_QUIT_QUEUE,           															///< 離開局中
+	PL_STATE_LOGOUT_GS,																											///< 登出GS
+	PL_STATE_MAX,
 }
