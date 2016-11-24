8f9f66110fc9dd155e74591f7fb6806142b7f87d
fanntone <fanntone@gmail.com>
2016-11-24 13:58:23 +0800

    Impelement Logic_CGGSLMSG_MAX.java.

--- SlotGame/src/com/jinglei/game/server/logic/Logic_CGGSLMSG_MAX.java
+++ SlotGame/src/com/jinglei/game/server/logic/Logic_CGGSLMSG_MAX.java
@@ -1,26 +1,34 @@
 package com.jinglei.game.server.logic;
 
 import com.jinglei.channel.NettyClientChannel;
+import com.jinglei.game.SysLog;
 import com.jinglei.server.logic.CommonLogic;
 
 public class Logic_CGGSLMSG_MAX implements CommonLogic {
 
 	@Override
-	public void executeLogic(byte[] arg0, NettyClientChannel arg1) {
-		// TODO Auto-generated method stub
-
+	public void executeLogic(byte[] packet_data, NettyClientChannel channel) {
+		try	{
+			SysLog.PrintInfo("Logic_CGGSLMSG_MAX Run Start!!");
+		}
+		catch(Exception e) {
+			SysLog.PrintError("Logic_CGGSLMSG_MAX Run Error!!");
+		}
+		finally {
+			SysLog.PrintInfo("Logic_CGGSLMSG_MAX Run Finally!!");
+		}
 	}
 
 	@Override
 	public String getLogicName() {
 		// TODO Auto-generated method stub
-		return null;
+		return new String(this.getClass().getName());
 	}
 
 	@Override
 	public CommonLogic newInstance() {
 		// TODO Auto-generated method stub
-		return null;
+		return new Logic_CGGSLMSG_MAX();
 	}
 
 }
